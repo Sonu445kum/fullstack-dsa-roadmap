a6074a236b9aca372a35f53ce8e68243a04408ea
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-16 17:49:39 +0530

    solved addTwoString of Leetcode Problem

--- 3_JAVASCRIPTS/03_Strings/01_Questions.js
+++ 3_JAVASCRIPTS/03_Strings/01_Questions.js
@@ -247,3 +247,40 @@
 //     return words.join(" ");
 // };
 // console.log("Swap first and last:",swapFirstLastWord(str));
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+// Example 1:
+
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+// Example 2:
+
+// Input: num1 = "456", num2 = "77"
+// Output: "533"
+// Example 3:
+
+// Input: num1 = "0", num2 = "0"
+// Output: "0"
+
+function addTwoString(num1, num2) {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
+  let result = "";
+  while (i >= 0 || j >= 0 || carry > 0) {
+    let digit1 = i >= 0 ? num1[i] - '0' : 0;
+    let digit2 = j >= 0 ? num2[j] - '0' : 0;
+    let sum = digit1 + digit2 + carry;
+
+    result = (sum % 10) + result;
+    carry = Math.floor(sum / 10);
+    i--;
+    j--;
+  }
+  return result;
+}
+let num1 = "121";
+let num2 = "121";
+console.log("The Sum Of the Two String:", addTwoString(num1, num2));
