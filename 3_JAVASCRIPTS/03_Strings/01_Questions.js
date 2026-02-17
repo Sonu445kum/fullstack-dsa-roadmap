b337a8f1a8aa80102b545fc339ace507c9558dc8
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-17 19:10:30 +0530

    solved anagram problems of the string

--- 3_JAVASCRIPTS/03_Strings/01_Questions.js
+++ 3_JAVASCRIPTS/03_Strings/01_Questions.js
@@ -264,23 +264,41 @@
 // Input: num1 = "0", num2 = "0"
 // Output: "0"
 
-function addTwoString(num1, num2) {
-  let i = num1.length - 1;
-  let j = num2.length - 1;
-  let carry = 0;
-  let result = "";
-  while (i >= 0 || j >= 0 || carry > 0) {
-    let digit1 = i >= 0 ? num1[i] - '0' : 0;
-    let digit2 = j >= 0 ? num2[j] - '0' : 0;
-    let sum = digit1 + digit2 + carry;
+// function addTwoString(num1, num2) {
+//   let i = num1.length - 1;
+//   let j = num2.length - 1;
+//   let carry = 0;
+//   let result = "";
+//   while (i >= 0 || j >= 0 || carry > 0) {
+//     let digit1 = i >= 0 ? num1[i] - '0' : 0;
+//     let digit2 = j >= 0 ? num2[j] - '0' : 0;
+//     let sum = digit1 + digit2 + carry;
 
-    result = (sum % 10) + result;
-    carry = Math.floor(sum / 10);
-    i--;
-    j--;
+//     result = (sum % 10) + result;
+//     carry = Math.floor(sum / 10);
+//     i--;
+//     j--;
+//   }
+//   return result;
+// }
+// let num1 = "121";
+// let num2 = "121";
+// console.log("The Sum Of the Two String:", addTwoString(num1, num2));
+
+// check the two string is anagram or not
+function isAnagram(s,t){
+  // base case
+  if(s.length !== t.length) return false;
+  let map ={};
+  for(let char of s){
+    map[char] = (map[char] || 0) + 1;
+  };
+  for(let char of t){
+    if(!map[char]) return false;
+    map[char]--;
   }
-  return result;
-}
-let num1 = "121";
-let num2 = "121";
-console.log("The Sum Of the Two String:", addTwoString(num1, num2));
+  return true;
+};
+let s="listen";
+let t="slient";
+console.log("The Two string is Anagram or not:",isAnagram(s,t));
