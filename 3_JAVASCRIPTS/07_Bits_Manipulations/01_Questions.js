22dd68786e67e0017dc0c135a64fbbdcb86abdd3
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-18 19:41:14 +0530

    solved xorOperations problems of bits manipulations

---
+++ 3_JAVASCRIPTS/07_Bits_Manipulations/01_Questions.js
@@ -0,0 +1,30 @@
+// You are given an integer n and an integer start.
+// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+// Return the bitwise XOR of all elements of nums.
+
+// Example 1:
+// Input: n = 5, start = 0
+// Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+// Where "^" corresponds to bitwise XOR operator.
+
+// Example 2:
+// Input: n = 4, start = 3
+// Output: 8
+// Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+
+function xorOperations(n, start) {
+  let emptyArr = [];
+  for (let i = 0; i < n; i++) {
+    emptyArr[i] = start + 2 * i;
+  }
+  let ans = emptyArr[0];
+  for (let i = 1; i < n; i++) {
+    ans = emptyArr[i] ^ ans;
+  }
+  return ans;
+}
+let n = 5,
+  start = 0;
+// let n= 4 , start = 3;
+console.log("The XorOperations:", xorOperations(n, start));
