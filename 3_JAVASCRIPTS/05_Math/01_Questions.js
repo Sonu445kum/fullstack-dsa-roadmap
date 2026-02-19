d1399290a7c8ffc2374d34d2d87ac9cc19af5df1
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-19 21:21:24 +0530

    solved the Perfect Number:

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -107,28 +107,82 @@
 // let num = 144;
 // console.log("The Number is Perfect or not:", isPerfectSquare(num));
 
-var mySqrt = function(num) {
-    if (num < 2) return num;
+// var mySqrt = function(num) {
+//     if (num < 2) return num;
 
-    let left = 1;
-    let right = Math.floor(num / 2);
-    let ans = 0;
+//     let left = 1;
+//     let right = Math.floor(num / 2);
+//     let ans = 0;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let square = mid * mid;
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//         let square = mid * mid;
 
-        if (square === num) {
-            return mid;
-        } else if (square < num) {
-            ans = mid;       // store possible answer
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+//         if (square === num) {
+//             return mid;
+//         } else if (square < num) {
+//             ans = mid;       // store possible answer
+//             left = mid + 1;
+//         } else {
+//             right = mid - 1;
+//         }
+//     }
+
+//     return ans;
+// };
+// let num = 4;
+// console.log("The Square Root of the Number:", mySqrt(num));
+
+// A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+
+// Given an integer n, return true if n is a perfect number, otherwise return false.
+
+// Example 1:
+
+// Input: num = 28
+// Output: true
+// Explanation: 28 = 1 + 2 + 4 + 7 + 14
+// 1, 2, 4, 7, and 14 are all divisors of 28.
+// Example 2:
+
+// Input: num = 7
+// Output: false
+// first approach:
+// var checkPerfectNumber = function(num) {
+//     let sum = 0;
+//     for(let i = 1; i < num; i++){
+//         if(num % i === 0){
+//             sum += i;
+//         }
+//     }
+//     if(sum === num){
+//         return true;
+//     }else{
+//         return false;
+//     }
+
+// };
+// let num = 6 ;
+// console.log("This Number is Perfect or not:",checkPerfectNumber(num));
+
+// 2nd Approach
+var checkPerfectNumber = function (num) {
+ 
+    if (num <= 1) return false;
+
+    let sum = 1; // 1 is always divisor (except 1 itself)
+
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            sum += i;
+
+            if (i !== num / i) {
+                sum += num / i;
+            }
         }
     }
 
-    return ans;
+    return sum === num;
 };
-let num = 4;
-console.log("The Square Root of the Number:", mySqrt(num));
+let num = 6;
+console.log("This Number is Perfect or not:", checkPerfectNumber(num));
