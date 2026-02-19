3d70846265c78e88b46c062dc72f8a45a7188cea
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-19 18:58:04 +0530

    find the longest word in the given array

--- 3_JAVASCRIPTS/03_Strings/01_Questions.js
+++ 3_JAVASCRIPTS/03_Strings/01_Questions.js
@@ -317,9 +317,44 @@
 // Input: s = "LOVELY"
 // Output: "lovely"
 
-var toLowerCase = function(s) {
-    let lowerCase =s.toLowerCase();
-    return lowerCase;
-};
-let s = "Hellow";
-console.log("The LowerCase of the String:",toLowerCase(s));
+// var toLowerCase = function(s) {
+//     let lowerCase =s.toLowerCase();
+//     return lowerCase;
+// };
+// let s = "Hellow";
+// console.log("The LowerCase of the String:",toLowerCase(s));
+
+// Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words.
+
+// If there is more than one possible answer, return the longest word with the smallest lexicographical order. If there is no answer, return the empty string.
+
+// Note that the word should be built from left to right with each additional character being added to the end of a previous word.
+
+// Example 1:
+// Input: words = ["w","wo","wor","worl","world"]
+// Output: "world"
+// Explanation: The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+// Example 2:
+// Input: words = ["a","banana","app","appl","ap","apply","apple"]
+// Output: "apple"
+// Explanation: Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+function findLongestWord(words) {
+  words.sort(); // lexicographical sort
+  let built = new Set([""]);
+  let max = "";
+
+  for (let word of words) {
+    if (built.has(word.slice(0, word.length - 1))) {
+      built.add(word);
+      if (word.length > max.length) {
+        max = word;
+      }
+    }
+  }
+
+  return max;
+}
+// let words = ["w","wo","wor","worl","world"];
+let words = ["a", "banana", "app", "appl", "ap", "apply", "apple"];
+console.log("The Longest Words:", findLongestWord(words));
