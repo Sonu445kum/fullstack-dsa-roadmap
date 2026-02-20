8456bc3b7c30d9ce662d6a01eb6e064030329877
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-20 17:41:04 +0530

    Solved 10 Different Problems of Loops

---
+++ 5_PYTHON/04_Loops/01_Questions.py
@@ -0,0 +1,71 @@
+# Print numbers from 1 to 10.
+# for i in range(1, 11):
+#     print(i, end=" ")
+
+# Print numbers from 10 to 1.
+# for i in range(10, 0,-1):
+#     print(i, end=" ")
+
+# Print even numbers from 1 to 50.
+# for i in range(1,50,1):
+#     if i % 2 == 0 :
+#         print(i ,end = " ")
+
+# Print odd numbers from 1 to 50.
+# for i in range(1,50,1):
+#     if i % 2 != 0 :
+#         print(i ,end = " ")
+
+
+# Print multiplication table of a given number.
+# num = int(input("Enter the Num:"))
+# for i in range( 1,11,1):
+#     print(num * i)
+
+
+# Find sum of first N natural numbers.
+# num = int(input("Enter the Num:"))
+# sum = 0
+# for i in range(1,num+1):
+#     sum+=i
+# print("The Sum of first N natural numbers: ",sum)
+
+# Find factorial of a number.
+# num = int(input("Enter the Num:"))
+# fact = 1
+# for i in range(1,num+1):
+#     fact *= i
+# print("The Factorial of given Num:",fact)
+
+# Count number of digits in a number.
+# num = int(input("Enter the num:"))
+# count = 0
+# while (num >=1):
+#     num = num/10
+#     count = count + 1
+
+# print("The Count Number of Digit:", count)
+
+
+# Reverse a number.
+# num = int(input("Enter the num:"))
+# rev = 0
+# while(num >=1):
+#     lastDigit = num % 10
+#     rev = (rev * 10) + lastDigit
+#     num = num //10
+# print("The Reverse of the Given Num:",rev)
+
+# Check if a number is palindrome.
+# num = int(input("Enter the num:"))
+# temp = num
+# rev = 0
+# while(temp >=1):
+#     lastDigit = temp % 10
+#     rev = (rev * 10) + lastDigit
+#     temp = temp //10
+# if rev == num:
+#     print("The number is Palindrome:",num)
+# else:
+#     print("The nummber is Not Palindrome:",num)
+
