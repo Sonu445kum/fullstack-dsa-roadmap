9347e9f9d92123c6e1ef25c24fd13b9fb994b3b5
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-20 17:00:57 +0530

    Solved 10 Problems of Conditional Statements

---
+++ 5_PYTHON/03_Conditional_Statements/01_Questions.py
@@ -0,0 +1,93 @@
+# Check if a number is even or odd.
+# num = int(input("Enter the Num:"))
+# if num % 2 == 0:
+#     print("The number is Even:",num)
+# else:
+#     print("The number is Odd:",num)
+
+# Check if a number is positive, negative, or zero.
+# num = int(input("Enter the Num:"))
+# if num > 0 :
+#     print("The Number is Positive:",num)
+# elif num == 0:
+#     print("The Number is Equal to Zero:",num)
+# else:
+#     print("The Number is Negative:",num)
+
+# Find the largest of two numbers.
+# num1 = int(input("Enter the First Number:"))
+# num2 = int(input("Enter the Second Number:"))
+
+# if num1 > num2 :
+#     print("Num1 is Greater than Num2:",num1)
+# else:
+#     print("Num2 is Greater than num1:",num2)
+
+# Find the largest of three numbers.
+# num1 = int(input("Enter the First Number:"))
+# num2 = int(input("Enter the Second Number:"))
+# num3 = int(input("Enter the third Number:"))
+
+# if ((num1 > num2) & (num1 > num3)):
+#     print("num1 is the greatest number:"), num1
+# elif ((num2 > num1) & (num2 > num3)):
+#     print("Num2 is the Greatest Number:", num2)
+# else:
+#     print("Num3 is the Greatest Number:", num3)
+
+# Check if a year is a leap year.
+# year = int(input("Enter the Year:"))
+
+# if ((year % 400 == 0) or ((year % 4 == 0) and (year % 100 !=0))):
+#     print("The Year is Leap Year:",year)
+# else:
+#     print("The Year is not Leap Year:",year)
+
+# Check if a character is vowel or consonant.
+# char = (input("Enter the Character:"))
+
+# if (char == 'a' or char == 'i' or char == 'o' or char == 'u' or char == 'e' or char == 'A' or char == 'I' or char == 'O' or char == 'U' or char == 'E' ):
+#     print("The Character is Vowel:",char)
+# else:
+#     print("The Character is Consonant:",char)
+
+# Check if a number is divisible by both 3 and 5.
+# num = int(input("Enter the Number:"))
+# if ((num % 5 ==0) and (num % 3 == 0)):
+#     print("The Number is Divisble by 3 and 5")
+# else:
+#     print("The Number is Not Divisble by 3 and 5")
+
+# Check if a number is a multiple of 7.
+# num = int(input("Enter a number: "))
+
+# if num % 7 == 0:
+#     print("The number is a multiple of 7:",num)
+# else:
+#     print("The number is not a multiple of 7:",num)
+
+# Check if a number is a 3-digit number.
+# a = int(input("Enter the Number:"))
+# if (a / 100 >=1):
+#     print("A is the three digit number:",a)
+# else:
+#     print("A is not a three digit number:",a)
+
+# Create a simple calculator using switch ( +, -, *, / ).
+# num1 = int(input("Enter the First Num:"))
+# num2 = int(input("Enter the Second Num:"))
+# operator = (input("Enter the operator(+,-,*,%,/):"))
+# match operator:
+#     case "+":
+#         print("Result:", (num1 + num2))
+#     case "-":
+#         print("Result:", (num1 - num2))
+#     case "*":
+#         print("Result:", (num1 * num2))
+#     case '%':
+#         print("Result:", (num1 % num2))
+#     case "/":
+#         if (num2 != 0):
+#             print("Result:", (num1/num2))
+#         else:
+#             print("Error: Division by zero is not allowed:")
