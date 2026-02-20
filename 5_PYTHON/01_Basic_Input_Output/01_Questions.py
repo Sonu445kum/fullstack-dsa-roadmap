c1ae6e99c06929eb86b401624cd9a2e616a252d6
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-20 15:38:21 +0530

    solved 10 problems of basic

---
+++ 5_PYTHON/01_Basic_Input_Output/01_Questions.py
@@ -0,0 +1,48 @@
+# Print "Hello World" on the console.
+print("Hello World..!!")
+
+# Take a number as input and print it.
+# num = int(input("Enter the Number:"))
+# print(num)
+
+
+# Take two numbers and print their sum.
+# num1 =int(input("Enter the First Number:"))
+# num2 =int(input("Enter the Second Number:"))
+# sum = num1 + num2
+# print("The Sum Of Two Num:",sum)
+
+# Take two numbers and print their product.
+# num1 =int(input("Enter the First Number:"))
+# num2 =int(input("Enter the Second Number:"))
+# product = num1 * num2
+# print("The Product of Two Number:",product)
+
+# Take a name as input and print: Hello <name>.
+# name =input("Enter Your Name:")
+# print("Hello",name)
+
+# Take a number and print its square.
+# num =int(input("Enter The Num:"))
+# print("Square of Given Num:",num*num)
+
+# Take a number and print its cube.
+# num =int(input("Enter The Num:"))
+# print("Cube of Given Num:",num*num*num)
+
+# Take two numbers and print their average.
+# num1 =int(input("Enter the First Number:"))
+# num2 =int(input("Enter the Second Number:"))
+# avg =(num1 + num2)/2
+# print("The Average of Given Num:",avg)
+
+# Convert temperature from Celsius to Fahrenheit.
+# celsius = int(input("Enter the temperature:"))
+# fahrenheit = (celsius*9/5) + 32
+# print("The Fahrenheit of given celsius:",fahrenheit)
+
+
+# Convert minutes into hours and minutes.
+# minutes =int(input("Enter the Minutes Here:"))
+# hour = 60/minutes
+# print("The Hour of Given Minutes:",hour)
