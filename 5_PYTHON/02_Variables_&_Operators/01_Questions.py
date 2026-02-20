d028821e929b66e6af95b264bed0ddfa15314d05
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-20 16:12:56 +0530

    Solved the 10 problems of Varables and Operators

---
+++ 5_PYTHON/02_Variables_&_Operators/01_Questions.py
@@ -0,0 +1,73 @@
+# Swap two numbers using a third variable.
+# a = int(input("Enter the First Num:"))
+# b = int(input("Enter the Second Num:"))
+# print("Before the swapping of number a and b :",a, b)
+# c = a
+# a = b
+# b = c
+# print("After the swapping of number a and b :",a, b)
+
+# Swap two numbers without using a third variable.
+# a = int(input("Enter the First Num:"))
+# b = int(input("Enter the Second Num:"))
+# print("Before the swapping of number a and b :", a, b)
+# a = a + b
+# b = a - b
+# a = a - b
+# print("After the swapping of number a and b :", a, b)
+
+
+# Find remainder when one number is divided by another.
+# a = int(input("Enter the First Num:"))
+# b = int(input("Enter the Second Num:"))
+# rem = a % b
+# print("The Reaminder of two num:", rem)
+
+# Check if a number is divisible by 5.
+# num = int(input("Enter the Number:"))
+# if num % 5 == 0:
+#     print("The number is divisible by 5")
+# else:
+#     print("The number is not divisible by 5")
+
+# Calculate simple interest.
+# p = int(input("Enter the Principle Amount:"))
+# r = int(input("Enter the rate:"))
+# t = int(input("Enter the time in year"))
+# Simple_Intreset =(p * r * t)/100
+# print("The Simple Intreset:",Simple_Intreset)
+
+# Calculate area of a rectangle.
+# Length = int(input("Enter the Length of Reactangel:"))
+# Breadth = int(input("Enter the Breadth of the Rectangle"))
+# Area = Length * Breadth 
+# print("The Area Of the Rectangle:",Area)
+
+# Calculate area of a circle.
+# radius = int(input("Enter the Radius of Circle:"))
+# area = float(22/7 * radius * radius)
+# print("Area of Circle:",area)
+
+# Calculate perimeter of a rectangle.
+# Length = int(input("Enter the Length of Reactangel:"))
+# Breadth = int(input("Enter the Breadth of the Rectangle"))
+# perimeter = 2 * ( Length + Breadth)
+# print("The Perimeter of the Rectangle:",perimeter)
+
+# Convert days into years, months, and days.
+# total_days = int(input("Enter number of days: "))
+
+# years = total_days // 365
+# remaining_days = total_days % 365
+
+# months = remaining_days // 30
+# days = remaining_days % 30
+
+# print("Years:", years)
+# print("Months:", months)
+# print("Days:", days)
+
+# Find the last digit of a number.
+# num = int(input("Enter the Number:121"))
+# lastDigit = num % 10
+# print("The LastDigit of Given Num:",lastDigit)
