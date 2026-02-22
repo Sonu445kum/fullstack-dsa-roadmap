e3008fb2bccf250710a6cfc15a40495092359f93
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-22 23:42:00 +0530

    findMaxConsecutiveOnes

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -354,17 +354,45 @@
 // Sum of digits = 4 + 4 + 2 + 1 = 11 
 // Result = 32 - 11 = 21
 
-var subtractProductAndSum = function(n) {
-    let product = 1,sum = 0;
-    while(n > 0){
-        let lastDigit = n % 10 ;
-        sum += lastDigit;
-        product *= lastDigit;
-        n = Math.floor(n/10);
-    };
-    return product - sum ;
+// var subtractProductAndSum = function(n) {
+//     let product = 1,sum = 0;
+//     while(n > 0){
+//         let lastDigit = n % 10 ;
+//         sum += lastDigit;
+//         product *= lastDigit;
+//         n = Math.floor(n/10);
+//     };
+//     return product - sum ;
 
     
+// };
+// let n = 234;
+// console.log("subtractProductAndSum:",subtractProductAndSum(n));
+
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+// Example 1:
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
+function findMaxConsecutiveOnes(nums){
+    let maxCount = 0;
+    let currentCount = 0 ;
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] === 1){
+            currentCount++;
+            maxCount = Math.max(maxCount , currentCount);
+        }else{
+            currentCount = 0;
+        }
+    }
+    return maxCount;
 };
-let n = 234;
-console.log("subtractProductAndSum:",subtractProductAndSum(n));
+let nums = [1,0,2,1,1,1,1];
+console.log("The Max Consecutive ones:",findMaxConsecutiveOnes(nums));
