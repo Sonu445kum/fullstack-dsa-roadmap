9a35db9abb5906d66d794484c8a3f37247f8edf2
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-22 23:12:06 +0530

    subtractProductAndSum

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -323,13 +323,48 @@
 // console.log("Number of Trailing Zeros are:", trailingZeroes(n));
 
 // 2nd Appraoch
-function trailingZeroes(n) {
-  let count = 0;
-  while (n > 0) {
-    n = Math.floor(n / 5);
-    count += n;
-  }
-  return count;
-}
-let n = 7;
-console.log("Number of Trailing Zeros are:", trailingZeroes(n));
+// function trailingZeroes(n) {
+//   let count = 0;
+//   while (n > 0) {
+//     n = Math.floor(n / 5);
+//     count += n;
+//   }
+//   return count;
+// }
+// let n = 7;
+// console.log("Number of Trailing Zeros are:", trailingZeroes(n));
+
+
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+// Example 1:
+
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+// Example 2:
+
+// Input: n = 4421
+// Output: 21
+// Explanation: 
+// Product of digits = 4 * 4 * 2 * 1 = 32 
+// Sum of digits = 4 + 4 + 2 + 1 = 11 
+// Result = 32 - 11 = 21
+
+var subtractProductAndSum = function(n) {
+    let product = 1,sum = 0;
+    while(n > 0){
+        let lastDigit = n % 10 ;
+        sum += lastDigit;
+        product *= lastDigit;
+        n = Math.floor(n/10);
+    };
+    return product - sum ;
+
+    
+};
+let n = 234;
+console.log("subtractProductAndSum:",subtractProductAndSum(n));
