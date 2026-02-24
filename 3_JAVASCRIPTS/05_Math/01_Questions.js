17020d563d42e8bfc20e68973c3e84549891e7f8
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-24 18:14:34 +0530

    solved the smallest Rnage

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -493,3 +493,29 @@
 // };
 // let arr = [1,4,2,5,3]
 // console.log("sumOddLengthSubarrays :",sumOddLengthSubarrays(arr))
+
+// You are given an integer array nums and an integer k.
+
+// In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+
+// The score of nums is the difference between the maximum and minimum elements in nums.
+
+// Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1], k = 0
+// Output: 0
+// Explanation: The score is max(nums) - min(nums) = 1 - 1 = 0.
+
+// var smallestRangeI = function(nums, k) {
+//     let minVal = Math.min(...nums);
+//     let maxVal = Math.max(...nums);
+
+//     return Math.max(0, maxVal - minVal - 2 * k);
+// };
+// let nums = [1], k = 0 ;
+// console.log(smallestRangeI(nums , k));
+
