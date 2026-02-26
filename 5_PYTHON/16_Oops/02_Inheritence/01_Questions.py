eede38a5d565569440dfb75ad2f6e9307cfadf94
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 21:51:48 +0530

    Solved the Multiple Inheritence

--- 5_PYTHON/16_Oops/02_Inheritence/01_Questions.py
+++ 5_PYTHON/16_Oops/02_Inheritence/01_Questions.py
@@ -10,3 +10,19 @@
 # c = Child()
 # print(c.show())
 # print(c.display())
+
+# Multiple Inheritance
+# class A:
+#     def method_a(self):
+#         return "A"
+
+# class B:
+#     def method_b(self):
+#         return "B"
+
+# class C(A, B):
+#     pass
+
+# obj = C()
+# print(obj.method_a())
+# print(obj.method_b())
