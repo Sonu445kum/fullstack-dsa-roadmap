6298660353098ff33e00abbe75e9192447d8139d
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 21:56:24 +0530

    Solved the @Property

--- 5_PYTHON/16_Oops/03_Encapsulations/01_Questions.py
+++ 5_PYTHON/16_Oops/03_Encapsulations/01_Questions.py
@@ -1,16 +1,35 @@
 # Encapsulation
-class Employee:
-    def __init__(self, salary):
-        self.__salary = salary
+# Private variable with getter/setter
+# class Employee:
+#     def __init__(self, salary):
+#         self.__salary = salary
 
-    def get_salary(self):
-        return self.__salary
+#     def get_salary(self):
+#         return self.__salary
 
-    def set_salary(self, amount):
-        if amount > 0:
-            self.__salary = amount
+#     def set_salary(self, amount):
+#         if amount > 0:
+#             self.__salary = amount
 
-e = Employee(50000)
-print(e.get_salary())
-e.set_salary(60000)
-print(e.get_salary())
+# e = Employee(50000)
+# print(e.get_salary())
+# e.set_salary(60000)
+# print(e.get_salary())
+
+# Using @property
+class Temperature:
+    def __init__(self, celsius):
+        self._celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, value):
+        if value < -273:
+            raise ValueError("Too cold!")
+        self._celsius = value
+
+t = Temperature(25)
+print(t.celsius)
