f5b117d56f888a82b69409348d4d199d8cda325e
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 23:26:56 +0530

    Solved isSameAfterReversals

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -669,18 +669,70 @@
 // Step 5) 4 is even, divide by 2 and obtain 2. 
 // Step 6) 2 is even, divide by 2 and obtain 1. 
 
-function findNumStep(s){
-    let step =0;
-    let carry =0;
-    for (let i = s.length - 1; i > 0; i--) {
-        if ((Number(s[i]) + carry) % 2 === 0) {
-            step += 1; // even → divide
-        } else {
-            step += 2; // odd → add + divide
-            carry = 1;
-        }
+// function findNumStep(s){
+//     let step =0;
+//     let carry =0;
+//     for (let i = s.length - 1; i > 0; i--) {
+//         if ((Number(s[i]) + carry) % 2 === 0) {
+//             step += 1; // even → divide
+//         } else {
+//             step += 2; // odd → add + divide
+//             carry = 1;
+//         }
+//     }
+//     return step + carry;
+// }
+// let s ="1101";
+// console.log("The Number of Steps to Reduce 1:",findNumStep(s));
+
+// 2119. A Number After a Double Reversal
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Reversing an integer means to reverse all its digits.
+
+// For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+// Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+
+ 
+
+// Example 1:
+
+// Input: num = 526
+// Output: true
+// Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+// Example 2:
+
+// Input: num = 1800
+// Output: false
+// Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+// Example 3:
+
+// Input: num = 0
+// Output: true
+// Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
+
+var isSameAfterReversals = function(num) {
+    let sum =0;
+    let newTemp = num;
+    while(newTemp > 0){
+        sum = sum*10 + newTemp % 10 ;
+        newTemp = Math.floor(newTemp/10)
     }
-    return step + carry;
-}
-let s ="1101";
-console.log("The Number of Steps to Reduce 1:",findNumStep(s));
+    let newSum = 0;
+    let temp = sum ;
+    while(temp > 0){
+        newSum = newSum* 10 + temp % 10 ;
+        temp = Math.floor(temp / 10);
+    }
+    if(num === newSum){
+        return true;
+    }else{
+        return false;
+    }
+};
+let num = 526;
+console.log("The Double Reverse Num:",isSameAfterReversals(num))
