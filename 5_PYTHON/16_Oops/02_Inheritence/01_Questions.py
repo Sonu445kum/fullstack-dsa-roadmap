a72bf9caf0728d7cfea5f98af934813a1bc9305f
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 21:50:35 +0530

    Solved the Single Inheritence

---
+++ 5_PYTHON/16_Oops/02_Inheritence/01_Questions.py
@@ -0,0 +1,12 @@
+# Single Inheritance
+# class Parent:
+#     def show(self):
+#         return "Parent class"
+
+# class Child(Parent):
+#     def display(self):
+#         return "Child class"
+
+# c = Child()
+# print(c.show())
+# print(c.display())
