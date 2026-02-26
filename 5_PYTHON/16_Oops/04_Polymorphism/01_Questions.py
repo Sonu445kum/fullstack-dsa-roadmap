6cf921354132b77b5e4aee514902d3fd53a2ef1a
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 21:48:22 +0530

    Operator Overloading

--- 5_PYTHON/16_Oops/04_Polymorphism/01_Questions.py
+++ 5_PYTHON/16_Oops/04_Polymorphism/01_Questions.py
@@ -1,14 +1,30 @@
 # Method Overriding
-class Animal:
-    def sound(self):
-        return "Animal sound"
+# class Animal:
+#     def sound(self):
+#         return "Animal sound"
 
-class Dog(Animal):
-    def sound(self):
-        return "Bark"
+# class Dog(Animal):
+#     def sound(self):
+#         return "Bark"
 
-a = Animal()
-d = Dog()
+# a = Animal()
+# d = Dog()
 
-print(a.sound())
-print(d.sound())
+# print(a.sound())
+# print(d.sound())
+
+# Operator Overloading
+# class Vector:
+#     def __init__(self, x):
+#         self.x = x
+
+#     def __add__(self, other):
+#         return Vector(self.x + other.x)
+
+#     def __str__(self):
+#         return str(self.x)
+
+# v1 = Vector(10)
+# v2 = Vector(20)
+
+# print(v1 + v2)
