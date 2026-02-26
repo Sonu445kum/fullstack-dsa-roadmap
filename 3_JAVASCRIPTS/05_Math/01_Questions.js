32e6623b3cedc043c19dc3260ec8a7c9d786ecb5
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-26 23:43:36 +0530

    Solved the Count Operations to Reduce Zero

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -715,24 +715,62 @@
 // Output: true
 // Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
 
-var isSameAfterReversals = function(num) {
-    let sum =0;
-    let newTemp = num;
-    while(newTemp > 0){
-        sum = sum*10 + newTemp % 10 ;
-        newTemp = Math.floor(newTemp/10)
+// var isSameAfterReversals = function(num) {
+//     let sum =0;
+//     let newTemp = num;
+//     while(newTemp > 0){
+//         sum = sum*10 + newTemp % 10 ;
+//         newTemp = Math.floor(newTemp/10)
+//     }
+//     let newSum = 0;
+//     let temp = sum ;
+//     while(temp > 0){
+//         newSum = newSum* 10 + temp % 10 ;
+//         temp = Math.floor(temp / 10);
+//     }
+//     if(num === newSum){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// };
+// let num = 526;
+// console.log("The Double Reverse Num:",isSameAfterReversals(num))
+
+// You are given two non-negative integers num1 and num2.
+
+// In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
+
+// For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
+// Return the number of operations required to make either num1 = 0 or num2 = 0.
+
+ 
+
+// Example 1:
+
+// Input: num1 = 2, num2 = 3
+// Output: 3
+// Explanation: 
+// - Operation 1: num1 = 2, num2 = 3. Since num1 < num2, we subtract num1 from num2 and get num1 = 2, num2 = 3 - 2 = 1.
+// - Operation 2: num1 = 2, num2 = 1. Since num1 > num2, we subtract num2 from num1.
+// - Operation 3: num1 = 1, num2 = 1. Since num1 == num2, we subtract num2 from num1.
+// Now num1 = 0 and num2 = 1. Since num1 == 0, we do not need to perform any further operations.
+// So the total number of operations required is 3.
+
+var countOperations = function(num1, num2) {
+    let operations = 0;
+    let temp1 = num1,temp2 =num2;
+    while(temp1 !== 0 && temp2 !== 0){
+        if(temp1 > temp2){
+            temp1 = temp1 - temp2 ;
+            operations++;
+        }else{
+            temp2 = temp2 - temp1;
+            operations++;
+        }
     }
-    let newSum = 0;
-    let temp = sum ;
-    while(temp > 0){
-        newSum = newSum* 10 + temp % 10 ;
-        temp = Math.floor(temp / 10);
-    }
-    if(num === newSum){
-        return true;
-    }else{
-        return false;
-    }
+    return operations;
 };
-let num = 526;
-console.log("The Double Reverse Num:",isSameAfterReversals(num))
+// let num1 = 5 , num2 = 4;
+let num1 = 2, num2 = 3 ;
+console.log("The Count Operartions to Reduce Zero:",countOperations(num1 , num2));
