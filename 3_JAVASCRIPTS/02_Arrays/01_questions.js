350a870d9a5c0b1a770e1ac6909047f763ddc205
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-01 15:33:52 +0530

    Solve umIdenticalPairs in O(n)

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -431,14 +431,29 @@
 // Input: nums = [1,2,3]
 // Output: 0
 
+// var numIdenticalPairs = function(nums) {
+//     let count = 0;
+//     for(let i = 0 ; i <nums.length; i++){
+//         for(let j = i+1; j<nums.length ; j++){
+//             if(nums[i] === nums[j] && i < j){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// };
+// let nums = [1,2,3,1,1,3];
+// console.log("The Number of Goods Pair:",numIdenticalPairs(nums));
+//  T.C => O(n*n);
+
 var numIdenticalPairs = function(nums) {
     let count = 0;
-    for(let i = 0 ; i <nums.length; i++){
-        for(let j = i+1; j<nums.length ; j++){
-            if(nums[i] === nums[j] && i < j){
-                count++;
-            }
+    let freq = {};
+    for(let num of nums){
+        if(freq[num]){
+            count += freq[num];
         }
+        freq[num] = (freq[num] | 0) + 1;
     }
     return count;
 };
