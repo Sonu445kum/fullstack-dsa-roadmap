ef28d723d14538e28c3d7783cdc7c4334e42c21d
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-02 23:51:58 +0530

    Solved the sumOfUnique

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -460,4 +460,40 @@
 // let nums = [1,2,3,1,1,3];
 // console.log("The Number of Goods Pair:",numIdenticalPairs(nums));
 
+// You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
 
+// Return the sum of all the unique elements of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: 0
+// Explanation: There are no unique elements, and the sum is 0.
+// Example 3:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+var sumOfUnique = function(nums) {
+    let freq = {};
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1;
+    }
+    let sum = 0;
+    for(let [key, value] of Object.entries(freq)){
+        if(value === 1){
+            sum +=Number(key);
+        }
+    }
+    return sum;
+};
+let nums = [1,1,1,1,1];
+console.log("SumOfUnique:",sumOfUnique(nums))
