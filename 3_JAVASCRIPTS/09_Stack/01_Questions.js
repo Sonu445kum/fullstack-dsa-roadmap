b601d01d5603a96e84ceb3fdc43894819a7124b0
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-15 23:56:09 +0530

    Define Stack

--- 3_JAVASCRIPTS/09_Stack/01_Questions.js
+++ 3_JAVASCRIPTS/09_Stack/01_Questions.js
@@ -42,45 +42,84 @@
 
 // console.log(preorderTraversal(root));
 
-var binaryTreePaths = function(root) {
-    let result = [];
+// var binaryTreePaths = function(root) {
+//     let result = [];
     
-    function dfs(node, path) {
-        if (!node) return;
+//     function dfs(node, path) {
+//         if (!node) return;
         
-        // Add current node to path
-        path += node.val;
+//         // Add current node to path
+//         path += node.val;
         
-        // If leaf node → save path
-        if (!node.left && !node.right) {
-            result.push(path);
-            return;
-        }
+//         // If leaf node → save path
+//         if (!node.left && !node.right) {
+//             result.push(path);
+//             return;
+//         }
         
-        // Continue DFS
-        path += "->";
-        dfs(node.left, path);
-        dfs(node.right, path);
-    }
+//         // Continue DFS
+//         path += "->";
+//         dfs(node.left, path);
+//         dfs(node.right, path);
+//     }
     
-    dfs(root, "");
-    return result;
-};
-class TreeNode {
-    constructor(val, left = null, right = null) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+//     dfs(root, "");
+//     return result;
+// };
+// class TreeNode {
+//     constructor(val, left = null, right = null) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+// let root = new TreeNode(
+//     1,
+//     null,
+//     new TreeNode(
+//         2,
+//         new TreeNode(3),
+//         null
+//     )
+// );
+// console.log(binaryTreePaths(root))
+
+class Stack {
+  constructor(){
+    this.items = [];
+  }
+
+  push(element){
+    this.items.push(element);
+  }
+
+  pop(){
+    if(this.isEmpty()){
+        return "Stack is empty";
     }
+    return this.items.pop();
+  }
+
+  peek(){
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty(){
+    return this.items.length === 0;
+  }
+
+  size(){
+    return this.items.length;
+  }
 }
 
-let root = new TreeNode(
-    1,
-    null,
-    new TreeNode(
-        2,
-        new TreeNode(3),
-        null
-    )
-);
-console.log(binaryTreePaths(root))
+let stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.peek()); // 30
+console.log(stack.pop());  // 30
+console.log(stack.size()); // 2
