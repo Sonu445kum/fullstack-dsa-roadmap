9d028327d21a0cc911190490b7f813e3e1d01e38
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-16 19:07:17 +0530

    Solved:intersectionTwoArray

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -667,18 +667,47 @@
 // Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
 // Output: false
 
-var arrayStringsAreEqual = function(word1, word2) {
-    let sum1 ="",sum2 ="";
-    // find the sum1 for word1
-    for(let num of word1){
-        sum1 += num;
+// var arrayStringsAreEqual = function(word1, word2) {
+//     let sum1 ="",sum2 ="";
+//     // find the sum1 for word1
+//     for(let num of word1){
+//         sum1 += num;
+//     }
+//     // find the sum2 for word2
+//     for(let num of word2){
+//         sum2 += num;
+//     }
+//     return sum1 === sum2;
+// };
+// // let word1 = ["ab", "c"], word2 = ["a", "bc"]; // true
+// let word1 = ["a", "cb"], word2 = ["ab", "c"]; // false
+// console.log("arrayStringsAreEqual:",arrayStringsAreEqual(word1 , word2));
+
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [9,4]
+// Explanation: [4,9] is also accepted.
+
+function intersectionTwoArray(nums1, nums2){
+    // create a set object
+    let set = new Set(nums1);
+    let result = new Set();
+
+    for(let num of nums2){
+        if(set.has(num)){
+            result.add(num)
+        }
     }
-    // find the sum2 for word2
-    for(let num of word2){
-        sum2 += num;
-    }
-    return sum1 === sum2;
+    return [...result];
 };
-// let word1 = ["ab", "c"], word2 = ["a", "bc"]; // true
-let word1 = ["a", "cb"], word2 = ["ab", "c"]; // false
-console.log("arrayStringsAreEqual:",arrayStringsAreEqual(word1 , word2));
+let nums1 = [4,9,5], nums2 = [9,4,9,8,4];
+console.log("Interscetions Of Two Array:",intersectionTwoArray(nums1,nums2));
