f3d4f2f1e2b34379c8f9a9a2fe576207e57260c6
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-16 15:31:26 +0530

    Solved:getXorSum

--- 3_JAVASCRIPTS/07_Bits_Manipulations/01_Questions.js
+++ 3_JAVASCRIPTS/07_Bits_Manipulations/01_Questions.js
@@ -117,3 +117,35 @@
 // let nums =[2,2,2,3,3,3,4,4,4,5];
 // console.log("The unique num:",findUnique(nums));
 
+// The XOR sum of a list is the bitwise XOR of all its elements. If the list only contains one element, then its XOR sum will be equal to this element.
+
+// For example, the XOR sum of [1,2,3,4] is equal to 1 XOR 2 XOR 3 XOR 4 = 4, and the XOR sum of [3] is equal to 3.
+// You are given two 0-indexed arrays arr1 and arr2 that consist only of non-negative integers.
+
+// Consider the list containing the result of arr1[i] AND arr2[j] (bitwise AND) for every (i, j) pair where 0 <= i < arr1.length and 0 <= j < arr2.length.
+
+// Return the XOR sum of the aforementioned list.
+
+ 
+
+// Example 1:
+
+// Input: arr1 = [1,2,3], arr2 = [6,5]
+// Output: 0
+// Explanation: The list = [1 AND 6, 1 AND 5, 2 AND 6, 2 AND 5, 3 AND 6, 3 AND 5] = [0,1,2,0,2,1].
+// The XOR sum = 0 XOR 1 XOR 2 XOR 0 XOR 2 XOR 1 = 0.
+
+// Brute Force
+function getXorSum(arr1,arr2){
+    let sum =0;
+    for(let i=0; i<arr1.length; i++){
+        for(let j=0; j<arr2.length; j++){
+            sum ^= (arr1[i] & arr2[j]);
+        }
+    }
+    return sum;
+};
+// let arr1 = [1,2,3], arr2 = [6,5]; // output ->0
+let arr1 =[12],arr2 =[4]; // output -> 4
+console.log("The XorSum:",getXorSum(arr1, arr2));
+
