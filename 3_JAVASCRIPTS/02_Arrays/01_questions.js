adc9ca798735e01bd06903da6cb2e6081d3b0fe8
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-19 23:58:49 +0530

    Solved:twoSumSortedArray

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -804,18 +804,53 @@
 
 // Output: [1,2]
 
-function topKFrequent(nums,k){
-    let freq = {};
-    for(let num of nums){
-        freq[num] = (freq[num] || 0) + 1;
+// function topKFrequent(nums,k){
+//     let freq = {};
+//     for(let num of nums){
+//         freq[num] = (freq[num] || 0) + 1;
+//     }
+//     // sort the value of Freq
+//     let sortedNum =Object.entries(freq).sort((a,b)=>b[1]-a[1]);
+//     let ans = [];
+//     for(let i = 0; i<k; i++){
+//         ans.push(Number(sortedNum[i][0]))
+//     }
+//     return ans;
+// }
+// nums = [1,2,1,2,1,2,3,1,3,2], k = 2;
+// console.log("topKfrequent:",topKFrequent(nums,k));
+
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+// Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+function twoSum(numbers , target){
+    let n = numbers.length; 
+    let left = 0;
+    let right = n -1;
+    while(left < right){
+        let sum = numbers[left] + numbers[right];
+        // check sum === target
+        if(sum === target){
+            return[left+1 , right+1];
+        }else if(sum < target){
+            left++;
+        }else{
+            right--;
+        }
     }
-    // sort the value of Freq
-    let sortedNum =Object.entries(freq).sort((a,b)=>b[1]-a[1]);
-    let ans = [];
-    for(let i = 0; i<k; i++){
-        ans.push(Number(sortedNum[i][0]))
-    }
-    return ans;
-}
-nums = [1,2,1,2,1,2,3,1,3,2], k = 2;
-console.log("topKfrequent:",topKFrequent(nums,k));
+};
+numbers = [2,7,11,15], target = 9;
+console.log("TwoSum:",twoSum(numbers,target));
