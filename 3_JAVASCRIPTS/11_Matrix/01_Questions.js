a13c6b8b32fa67c44add9ff244d79dd676acfe05
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-20 23:54:39 +0530

    SOlved:Multiply Two Matrix

--- 3_JAVASCRIPTS/11_Matrix/01_Questions.js
+++ 3_JAVASCRIPTS/11_Matrix/01_Questions.js
@@ -1,10 +1,30 @@
-function addMatrix(A, B) {
-  let result = [];
+// function addMatrix(A, B) {
+//   let result = [];
+
+//   for (let i = 0; i < A.length; i++) {
+//     result[i] = [];
+//     for (let j = 0; j < A[0].length; j++) {
+//       result[i][j] = A[i][j] + B[i][j];
+//     }
+//   }
+
+//   return result;
+// }
+// let A = [[1, 2, 3]];
+// let B = [[4, 5, 6]];
+
+// console.log("A+B:", addMatrix(A, B));
+
+function multiplyMatrix(A, B) {
+  let result = Array.from({ length: A.length }, () =>
+    Array(B[0].length).fill(0)
+  );
 
   for (let i = 0; i < A.length; i++) {
-    result[i] = [];
-    for (let j = 0; j < A[0].length; j++) {
-      result[i][j] = A[i][j] + B[i][j];
+    for (let j = 0; j < B[0].length; j++) {
+      for (let k = 0; k < B.length; k++) {
+        result[i][j] += A[i][k] * B[k][j];
+      }
     }
   }
 
@@ -13,4 +33,5 @@ function addMatrix(A, B) {
 let A = [[1, 2, 3]];
 let B = [[4, 5, 6]];
 
-console.log("A+B:", addMatrix(A, B));
+console.log("A*B:", multiplyMatrix(A, B));
+
