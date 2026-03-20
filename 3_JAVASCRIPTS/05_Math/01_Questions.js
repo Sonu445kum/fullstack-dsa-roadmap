ddb4d7a0486e82cb9dc7a328e067069dece92b08
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-20 16:00:37 +0530

    Solved:minMoves

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -1625,3 +1625,34 @@
 // };
 // nums = [0,10], k = 2;
 // console.log("SmallestRangeII:",smallestRangeII(nums , k));
+
+
+// Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+// In one move, you can increment n - 1 elements of the array by 1.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 3
+// Explanation: Only three moves are needed (remember each move increments two elements):
+// [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+// Example 2:
+
+// Input: nums = [1,1,1]
+// Output: 0
+
+var minMoves = function(nums) {
+    let min = Math.min(...nums);
+    let moves =0;
+    for(let num of nums){
+        moves += (num - min)
+    }
+    return moves;
+
+    
+};
+let nums = [1,2,3];
+console.log("Minimum Moves:",minMoves(nums))
+
