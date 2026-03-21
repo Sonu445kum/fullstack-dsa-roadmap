0e2f95f6ae72044fa5ed12374dc993fb4d643c88
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-21 19:30:37 +0530

    solved:mostFrequentEven

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -910,18 +910,80 @@
 // Input: nums = [0,1]
 // Output: [1,0]
 
-var singleNumber = function(nums) {
-   let freq = {};
-   for(let num of nums){
-    freq[num] = (freq[num] || 0) + 1;
-   };
-   let arr = [];
-   for(let [key,value] of Object.entries(freq)){
-    if(value === 1){
-        arr.push(Number(key));
+// 1st Appraoch;
+// var singleNumber = function(nums) {
+//    let freq = {};
+//    for(let num of nums){
+//     freq[num] = (freq[num] || 0) + 1;
+//    };
+//    let arr = [];
+//    for(let [key,value] of Object.entries(freq)){
+//     if(value === 1){
+//         arr.push(Number(key));
+//     }
+//    }
+   
+//    return arr;
+// };
+// let nums = [1,2,1,3,2,5];
+// console.log("SingleNumber:",singleNumber(nums)); // t.c->18ms
+
+// 2nd Approach -> optimal
+// var singleNumber = function(nums) {
+//     let freq = {};
+    
+//     for(let num of nums){
+//         freq[num] = (freq[num] || 0) + 1;
+//     }
+
+//     let result = [];
+//     for(let key in freq){
+//         if(freq[key] === 1){
+//             result.push(Number(key));
+//         }
+//     }
+
+//     return result;
+// };
+// let nums = [1,2,1,3,2,5];
+// console.log("SingleNumber:",singleNumber(nums)); // t.c->13ms
+
+// Given an integer array nums, return the most frequent even element.
+
+// If there is a tie, return the smallest one. If there is no such element, return -1.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,2,2,4,4,1]
+// Output: 2
+// Explanation:
+// The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+// We return the smallest one, which is 2.
+// Example 2:
+
+// Input: nums = [4,4,4,9,2,4]
+// Output: 4
+// Explanation: 4 is the even element appears the most.
+
+var mostFrequentEven = function(nums) {
+    let freq = {};
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1;
+    };
+    let mostFreq =0;
+    let result = -1;
+    for(let key in freq){
+        let num = Number(key);
+        if(num % 2 === 0){
+            if(freq[key] > mostFreq || freq[key] === mostFreq && num < result){
+                mostFreq = freq[key];
+                result = num;
+            }
+        }
     }
-   }
-   return arr;
+    return result;
 };
-let nums = [1,2,1,3,2,5];
-console.log("SingleNumber:",singleNumber(nums));
+let nums = [4,4,4,9,2,4];
+console.log("MostFrequentEven:",mostFrequentEven(nums))
