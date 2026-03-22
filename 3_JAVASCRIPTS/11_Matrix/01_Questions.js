9cc0be309b8dc58c10976c3bc908dcf3b8971f17
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-22 17:38:22 +0530

    Solved:construct2D Array

--- 3_JAVASCRIPTS/11_Matrix/01_Questions.js
+++ 3_JAVASCRIPTS/11_Matrix/01_Questions.js
@@ -35,18 +35,39 @@
 
 // console.log("A*B:", multiplyMatrix(A, B));
 
-function transposeMatrix(matrix){
-  let row = matrix.length;
-  let cols = matrix[0].length;
-  let result =[];
-  for(let i=0; i<cols; i++){
-    result[i] = [];
-    for(let j=0; j<row; j++){
-      result[i][j] = matrix[j][i];
-    }
-  }
-  return result;
-};
-let matrix = [[1,2,3],[4,5,6],[7,8,9]];
-console.log("Tranpose of a matrix:",transposeMatrix(matrix));
+// function transposeMatrix(matrix){
+//   let row = matrix.length;
+//   let cols = matrix[0].length;
+//   let result =[];
+//   for(let i=0; i<cols; i++){
+//     result[i] = [];
+//     for(let j=0; j<row; j++){
+//       result[i][j] = matrix[j][i];
+//     }
+//   }
+//   return result;
+// };
+// let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+// console.log("Tranpose of a matrix:",transposeMatrix(matrix));
 
+// You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
+
+// The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+
+// Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+
+var construct2DArray = function(original, m, n) {
+    let twoDArr = [];
+    // base case
+    if(original.length !== m*n) return twoDArr;
+    let index =0;
+    for(let i =0; i< m; i++){
+        twoDArr[i] = [];
+        for(let j=0; j<n; j++){
+            twoDArr[i][j] = original[index++];
+        }
+    }
+    return twoDArr;
+};
+let original = [1,2,3,4], m = 2, n = 2;
+console.log("construct2DArray:",construct2DArray(original , m ,n));
