f2d9aae16c2ec5de452e0148ead01caebe2200a6
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-22 17:09:50 +0530

    Solved:smallestEqual

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1045,17 +1045,47 @@
 // Explanation: After sorting, nums is [1,2,2,3,5].
 // The index where nums[i] == 3 is 3.
 
-var targetIndices = function(nums, target) {
-    // sort the nums
-    nums.sort((a,b)=> a- b);
-    let ans =[];
+// var targetIndices = function(nums, target) {
+//     // sort the nums
+//     nums.sort((a,b)=> a- b);
+//     let ans =[];
 
-    for(let i =0; i<nums.length; i++){
-        if(nums[i] === target){
-            ans.push(i);
+//     for(let i =0; i<nums.length; i++){
+//         if(nums[i] === target){
+//             ans.push(i);
+//         }
+//     } 
+//     return ans; 
+// };
+// nums = [1,2,5,2,3], target = 3;
+// console.log("targetIndices:",targetIndices(nums,target));
+
+// Given a 0-indexed integer array nums, return the smallest index i of nums such that i mod 10 == nums[i], or -1 if such index does not exist.
+
+// x mod y denotes the remainder when x is divided by y.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,2]
+// Output: 0
+// Explanation: 
+// i=0: 0 mod 10 = 0 == nums[0].
+// i=1: 1 mod 10 = 1 == nums[1].
+// i=2: 2 mod 10 = 2 == nums[2].
+// All indices have i mod 10 == nums[i], so we return the smallest index 0.
+
+var smallestEqual = function(nums) {
+    let minIndex =0;
+    let result = -1;
+    for(let i=0; i<nums.length; i++){
+        if((i % 10 === nums[i]) && result < minIndex){
+            minIndex = i;
+            result = minIndex;
         }
-    } 
-    return ans; 
+    }
+    return result;
 };
-nums = [1,2,5,2,3], target = 3;
-console.log("targetIndices:",targetIndices(nums,target));
+let nums = [0,1,2];
+console.log("smallestEqual:",smallestEqual(nums));
