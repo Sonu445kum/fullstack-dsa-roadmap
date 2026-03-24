b99f848bd354e75d54ff2b240e053f372008d2a3
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-24 22:57:44 +0530

    Solved:numSubarraysWithSum

--- 3_JAVASCRIPTS/13_Preffix_Sum/01_Questions.js
+++ 3_JAVASCRIPTS/13_Preffix_Sum/01_Questions.js
@@ -11,22 +11,22 @@
 // let num =[1,2,3,4]
 
 // find the subarray sum with k
-var subarraySum = function (nums, k) {
-  let map = new Map();
-  map.set(0, 1);
-  let sum = 0;
-  let count = 0;
-  for (let num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
-};
-let nums = [4,5,0,-2,-3,1], k = 5;
-console.log("SubarraySum:",subarraySum(nums, k));
+// var subarraySum = function (nums, k) {
+//   let map = new Map();
+//   map.set(0, 1);
+//   let sum = 0;
+//   let count = 0;
+//   for (let num of nums) {
+//     sum += num;
+//     if (map.has(sum - k)) {
+//       count += map.get(sum - k);
+//     }
+//     map.set(sum, (map.get(sum) || 0) + 1);
+//   }
+//   return count;
+// };
+// let nums = [4,5,0,-2,-3,1], k = 5;
+// console.log("SubarraySum:",subarraySum(nums, k));
 
 // Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
 
@@ -43,22 +43,63 @@ console.log("SubarraySum:",subarraySum(nums, k));
 // Input: nums = [5], k = 9
 // Output: 0
 
-var subarraysDivByK = function (nums, k) {
-  let map = new Map();
-  map.set(0, 1);
-  let sum = 0;
-  let count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    let rem = sum % k;
-    // if the remainder is negative
-    if (rem < 0) rem += k;
-    if (map.has(rem)) {
-      count += map.get(rem);
+// var subarraysDivByK = function (nums, k) {
+//   let map = new Map();
+//   map.set(0, 1);
+//   let sum = 0;
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     sum += nums[i];
+//     let rem = sum % k;
+//     // if the remainder is negative
+//     if (rem < 0) rem += k;
+//     if (map.has(rem)) {
+//       count += map.get(rem);
+//     }
+//     map.set(rem, (map.get(rem) || 0) + 1);
+//   }
+//   return count;
+// };
+// ((nums = [4, 5, 0, -2, -3, 1]), (k = 5));
+// console.log("SubarrayDivByK:", subarraysDivByK(nums, k));
+
+// Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+// A subarray is a contiguous part of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,0,1,0,1], goal = 2
+// Output: 4
+// Explanation: The 4 subarrays are bolded and underlined below:
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// Example 2:
+
+// Input: nums = [0,0,0,0,0], goal = 0
+// Output: 15
+
+function numSubarraysWithSum(nums , goal){
+    let map = new Map();
+    map.set(0 , 1);
+    let count = 0;
+    let sum = 0;
+    for(let num of nums){
+        sum += num;
+
+        // check sum - goal is already present or not in map
+        // if the sum - goal is present ,then incrment the count
+        if(map.has(sum - goal)){
+            count += map.get(sum - goal);
+        }
+        map.set(sum , (map.get(sum) || 0) + 1);
     }
-    map.set(rem, (map.get(rem) || 0) + 1);
-  }
-  return count;
+    return count;
 };
-((nums = [4, 5, 0, -2, -3, 1]), (k = 5));
-console.log("SubarrayDivByK:", subarraysDivByK(nums, k));
+let nums = [1,0,1,0,1], goal = 2;
+console.log("numSubarraysWithSum:",numSubarraysWithSum(nums , goal));
+
