4f02a5b2a0abb1d8461726c083ad55ec3ca6e2fb
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-25 22:11:05 +0530

    Solved:firstUniqueCharacter

--- 3_JAVASCRIPTS/03_Strings/01_Questions.js
+++ 3_JAVASCRIPTS/03_Strings/01_Questions.js
@@ -533,3 +533,40 @@
 // }
 // ;let s = "Let's take LeetCode contest";
 // console.log("Reverse Word:",reverseWord(s));
+
+
+
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetcode"
+
+// Output: 0
+
+// Explanation:
+
+// The character 'l' at index 0 is the first character that does not occur at any other index.
+
+// Example 2:
+
+// Input: s = "loveleetcode"
+
+// Output: 2
+
+function firstUniqChar(s){
+    let freq ={};
+    for(let char of s){
+        freq[char] =(freq[char] || 0) + 1;
+    }
+    for(let i =0; i<s.length; i++){
+       if(freq[s[i]] === 1){
+        return i;
+       }
+    }
+    return -1;
+};
+let s = "loveleetcode";
+console.log("FirstUniqueChar:",firstUniqChar(s));
