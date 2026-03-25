0a98065454038d8f008179e51ec537a44493065a
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-25 16:56:28 +0530

    Solved:checkPrimeFrequency

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -2049,18 +2049,84 @@
 // Input: nums = [1,10,2,9]
 // Output: 16
 
-function minMoves2(nums){
-    // sort the elements
-    // nums.sort((a,b)=>(a-b));
-    // find the median
-    let median = nums[Math.floor((nums.length)/2)];
-    let moves = 0;
+// function minMoves2(nums){
+//     // sort the elements
+//     // nums.sort((a,b)=>(a-b));
+//     // find the median
+//     let median = nums[Math.floor((nums.length)/2)];
+//     let moves = 0;
+//     for(let num of nums){
+//         moves += Math.abs(num - median);
+//     }
+//     return moves;
+// };
+// let nums = [1,2,3];
+// console.log("Minimum Moves to Equal All the Element:",minMoves2(nums));
+
+// You are given an integer array nums.
+
+// Return true if the frequency of any element of the array is prime, otherwise, return false.
+
+// The frequency of an element x is the number of times it occurs in the array.
+
+// A prime number is a natural number greater than 1 with only two factors, 1 and itself.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,4]
+
+// Output: true
+
+// Explanation:
+
+// 4 has a frequency of two, which is a prime number.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4,5]
+
+// Output: false
+
+// Explanation:
+
+// All elements have a frequency of one.
+
+// Example 3:
+
+// Input: nums = [2,2,2,4,4]
+
+// Output: true
+
+// Explanation:
+
+// Both 2 and 4 have a prime frequency.
+
+// first Appraoch
+function checkPrimeFrequency(nums){
+    let freq ={};
     for(let num of nums){
-        moves += Math.abs(num - median);
+        freq[num] = (freq[num] || 0) + 1;
     }
-    return moves;
-};
-let nums = [1,2,3];
-console.log("Minimum Moves to Equal All the Element:",minMoves2(nums));
+    for(let value of Object.entries(freq)){
+        if(isPrime(value)){
+            return true;
+        }
+    }
+    return false;
+}
+// functions for check prime
+function isPrime(n){
+    if(n <= 1) return false;
+    for(let i = 2; i*i <= n; i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+let  nums = [1,2,3,4,5,4];
+console.log("checkPrimeFrequency:",checkPrimeFrequency(nums));
 
 
