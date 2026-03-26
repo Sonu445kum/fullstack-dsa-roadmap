f580e05ed9df887da818fc38f227099ccdd6db76
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-26 15:28:40 +0530

    solved:countNicePairs

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -2238,16 +2238,64 @@
 // Both a and b are no-zero integers, and a + b = 11 = n.
 // Note that there are other valid answers as [8, 3] that can be accepted.
 
-function getNoZeroInteger(n){
-    function hasZero(num){
-        return num.toString().includes('0');
-    };
-    for(let a = 1; a<=n; a++){
-        let b = n - a;
-        if(!hasZero(a) && !hasZero(b)){
-            return [a,b];
+// function getNoZeroInteger(n){
+//     function hasZero(num){
+//         return num.toString().includes('0');
+//     };
+//     for(let a = 1; a<=n; a++){
+//         let b = n - a;
+//         if(!hasZero(a) && !hasZero(b)){
+//             return [a,b];
+//         }
+//     }
+// };
+// let n=11;
+// console.log("GetN0ZeroInteger:",getNoZeroInteger(n));
+
+
+// You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
+
+// 0 <= i < j < nums.length
+// nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+// Return the number of nice pairs of indices. Since that number can be too large, return it modulo 109 + 7.
+
+ 
+
+// Example 1:
+
+// Input: nums = [42,11,1,97]
+// Output: 2
+// Explanation: The two pairs are:
+//  - (0,3) : 42 + rev(97) = 42 + 79 = 121, 97 + rev(42) = 97 + 24 = 121.
+//  - (1,2) : 11 + rev(1) = 11 + 1 = 12, 1 + rev(11) = 1 + 11 = 12.
+// Example 2:
+
+// Input: nums = [13,10,35,24,76]
+// Output: 4
+
+function countNicePairs(nums){
+    let mod =1e9 + 7;
+    let map = new Map();
+    map.set(0,1);
+    let count = 0;
+    for(let num of nums){
+        let key = num - rev(num);
+        if(map.has(key)){
+            count = (count + (map.get(key)))%mod;
         }
+        map.set(key, (map.get(key)|| 0) + 1);
     }
-};
-let n=11;
-console.log("GetN0ZeroInteger:",getNoZeroInteger(n));
+    return count;
+}
+let nums = [13,10,35,24,76];
+console.log("countNicePairs:",countNicePairs(nums));
+function rev(n){
+let reverse =0;
+let temp = n;
+while(temp> 0){
+    let lastDigit = temp % 10;
+    reverse = reverse*10 + lastDigit;
+    temp = Math.floor(temp/10);
+}
+return reverse
+}
