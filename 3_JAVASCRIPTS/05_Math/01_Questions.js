4e02aa9fcfb203d45657cadf85909d56c56b96c7
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-28 00:04:32 +0530

    Solved:subsetsXorSum

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -2314,10 +2314,42 @@
 // Output: []
 // Explanation: There is no way to express 4 as the sum of 3 consecutive integers.
 
-var sumOfThree = function(num) {
-    if(num % 3 !== 0) return [];
-    let mid = num/3;
-    return [mid-1,mid,mid+1];
+// var sumOfThree = function(num) {
+//     if(num % 3 !== 0) return [];
+//     let mid = num/3;
+//     return [mid-1,mid,mid+1];
+// }
+// let num = 33;
+// console.log("sumOfThree:",sumOfThree(num));
+
+// The XOR total of an array is defined as the bitwise XOR of all its elements, or 0 if the array is empty.
+
+// For example, the XOR total of the array [2,5,6] is 2 XOR 5 XOR 6 = 1.
+// Given an array nums, return the sum of all XOR totals for every subset of nums. 
+
+// Note: Subsets with the same elements should be counted multiple times.
+
+// An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3]
+// Output: 6
+// Explanation: The 4 subsets of [1,3] are:
+// - The empty subset has an XOR total of 0.
+// - [1] has an XOR total of 1.
+// - [3] has an XOR total of 3.
+// - [1,3] has an XOR total of 1 XOR 3 = 2.
+// 0 + 1 + 3 + 2 = 6
+
+function subsetXORSum(nums){
+    let or  = 0;
+    for(let num of nums){
+        or |= num;
+    }
+    return or * (1 << (nums.length -1));
 }
-let num = 33;
-console.log("sumOfThree:",sumOfThree(num));
+let nums =[1,3];
+console.log("subsetsXorSum:",subsetXORSum(nums));
