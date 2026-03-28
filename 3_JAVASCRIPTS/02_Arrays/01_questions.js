2ea43a88a5dda050b3b9328db478ddb6a332379f
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-29 00:26:16 +0530

    Solved:countPairs

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1381,14 +1381,45 @@
 // Output: 1
 // Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.
 
-var getMinDistance = function(nums, target, start) {
-    let minDist = Infinity;
+// var getMinDistance = function(nums, target, start) {
+//     let minDist = Infinity;
+//     for(let i=0; i<nums.length; i++){
+//         if(nums[i] === target){
+//             minDist = Math.min(minDist, Math.abs(i- start));
+//         }
+//     }
+//     return minDist;
+// };
+// let nums = [1,2,3,4,5], target = 5, start = 3;
+// console.log("getMinDistance:",getMinDistance(nums , target , start));
+
+// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+ 
+
+// Example 1:
+
+// Input: nums = [3,1,2,2,2,1,3], k = 2
+// Output: 4
+// Explanation:
+// There are 4 pairs that meet all the requirements:
+// - nums[0] == nums[6], and 0 * 6 == 0, which is divisible by 2.
+// - nums[2] == nums[3], and 2 * 3 == 6, which is divisible by 2.
+// - nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
+// - nums[3] == nums[4], and 3 * 4 == 12, which is divisible by 2.
+
+var countPairs = function(nums, k) {
+    let count =0;
     for(let i=0; i<nums.length; i++){
-        if(nums[i] === target){
-            minDist = Math.min(minDist, Math.abs(i- start));
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] === nums[j]){
+                let ans = i*j;
+                if(ans % k===0){
+                    count++;
+                }
+            }
         }
     }
-    return minDist;
+    return count;
 };
-let nums = [1,2,3,4,5], target = 5, start = 3;
-console.log("getMinDistance:",getMinDistance(nums , target , start));
+let nums = [3,1,2,2,2,1,3], k = 2;
+console.log("CountPairs:",countPairs(nums , k));
