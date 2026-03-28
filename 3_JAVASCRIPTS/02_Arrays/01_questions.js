b285679f36c8b5629f466a5a0a798c0f3e2a186a
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-28 19:05:34 +0530

    Solved:frequcency

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1322,11 +1322,47 @@
 // Output: [1,2,3,4,5]
 // Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 
-var runningSum = function(nums) {
-    for(let i=1; i<nums.length; i++){
-        nums[i] = nums[i-1] + nums[i];
+// var runningSum = function(nums) {
+//     for(let i=1; i<nums.length; i++){
+//         nums[i] = nums[i-1] + nums[i];
+//     }
+//     return nums;
+// };
+// let nums = [1,2,3,4];
+// console.log("runnningSum:",runningSum(nums));
+
+
+// Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+// Return the sorted array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,2,2,2,3]
+// Output: [3,1,1,2,2,2]
+// Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+// Example 2:
+
+// Input: nums = [2,3,1,3,2]
+// Output: [1,3,3,2,2]
+// Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+
+function frequencySort(nums){
+    let freq = {};
+    // count the frequcency of Each Element
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1;
     }
+    // now sort the frequecncy
+    nums.sort((a,b)=>{
+        if(freq[a] === freq[b]){
+            return freq[b] - freq[a];
+        }
+        return freq[a] - freq[b];
+    })
     return nums;
-};
-let nums = [1,2,3,4];
-console.log("runnningSum:",runningSum(nums));
+}
+let nums = [1,1,2,2,2,3];
+console.log("frequcenySort:",frequencySort(nums));
