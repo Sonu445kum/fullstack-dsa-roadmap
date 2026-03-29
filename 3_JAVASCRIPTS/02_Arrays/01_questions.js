e8478471035e635fbf4c045bf91f563aa7c946a3
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-29 22:52:19 +0530

    Solved:average

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1626,17 +1626,52 @@
 // Input: arr = [1,1]
 // Output: 1
 
-var findSpecialInteger = function(arr) {
-    let freq =[];
-    for(let num of arr){
-        freq[num] = (freq[num] || 0) + 1;
+// var findSpecialInteger = function(arr) {
+//     let freq =[];
+//     for(let num of arr){
+//         freq[num] = (freq[num] || 0) + 1;
+//     }
+//     let n =arr.length;
+//     for(let [key,value] of Object.entries(freq)){
+//         if (value > n / 4) {
+//             return Number(key);
+//         } 
+//     }
+// };
+// let arr = [1,2,2,6,6,6,6,7,10];
+// console.log("FindsSpecialInteger:",findSpecialInteger(arr));
+
+// You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+
+// Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
+
+
+
+// Example 1:
+
+// Input: salary = [4000,3000,1000,2000]
+// Output: 2500.00000
+// Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+// Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
+// Example 2:
+
+// Input: salary = [1000,2000,3000]
+// Output: 2000.00000
+// Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+// Average salary excluding minimum and maximum salary is (2000) / 1 = 2000
+
+var average = function(salary) {
+    let n = salary.length;
+    let max = Math.max(...salary);
+    let min = Math.min(...salary);
+    let sum =0;
+    for(let num of salary){
+        sum += num;
     }
-    let n =arr.length;
-    for(let [key,value] of Object.entries(freq)){
-        if (value > n / 4) {
-            return Number(key);
-        } 
-    }
+    let total = min + max;
+    let avg = sum - total;
+    let diffrence = n - 2;
+    return avg/diffrence;
 };
-let arr = [1,2,2,6,6,6,6,7,10];
-console.log("FindsSpecialInteger:",findSpecialInteger(arr));
+let  salary = [4000,3000,1000,2000];
+console.log("average:",average(salary));
