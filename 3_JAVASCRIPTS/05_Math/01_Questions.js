2038cde811a9a47254b8fbfec39aad7f30300f7e
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-30 23:59:05 +0530

    Solved:nthUglyNumber

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -2630,3 +2630,37 @@ var intToRoman = function(num) {
 };
 let num = 3749;
 console.log("Integer To Roman:",intToRoman(num));
+
+var nthUglyNumber = function(n) {
+    let ugly = new Array(n).fill(0);
+    ugly[0] = 1;
+
+    let i2 = 0, i3 = 0, i5 = 0;
+
+    let next2 = 2, next3 = 3, next5 = 5;
+
+    for (let i = 1; i < n; i++) {
+        let nextUgly = Math.min(next2, next3, next5);
+        ugly[i] = nextUgly;
+
+        if (nextUgly === next2) {
+            i2++;
+            next2 = ugly[i2] * 2;
+        }
+
+        if (nextUgly === next3) {
+            i3++;
+            next3 = ugly[i3] * 3;
+        }
+
+        if (nextUgly === next5) {
+            i5++;
+            next5 = ugly[i5] * 5;
+        }
+    }
+
+    return ugly[n - 1];
+};
+
+let n = 10;
+console.log("nthUglyNumber:",nthUglyNumber(n));
