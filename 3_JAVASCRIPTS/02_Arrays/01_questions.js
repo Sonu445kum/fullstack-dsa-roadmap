93fc1f1372884215bb9e71cddf73abdb56f0c81f
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-04 23:44:52 +0530

    Target are present or not

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1818,13 +1818,43 @@
 // Input: nums = [4,5,6,7,0,1,2], target = 3
 // Output: -1
 
+// var search = function(nums, target) {
+//     for(let i =0; i<nums.length; i++){
+//         if(nums[i] === target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// };
+// let nums = [4,5,6,7,0,1,2], target = 0;
+// console.log("Search Element At Index:",search(nums , target)); 
+
+// There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+// Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+// Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+// You must decrease the overall operation steps as much as possible.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+// Output: true
+// Example 2:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+// Output: false
+
 var search = function(nums, target) {
     for(let i =0; i<nums.length; i++){
-        if(nums[i] === target){
-            return i;
+        if(nums [i] === target){
+            return true;
         }
     }
-    return -1;
+    return false;
 };
-let nums = [4,5,6,7,0,1,2], target = 0;
-console.log("Search Element At Index:",search(nums , target)); 
+let nums = [2,5,6,0,0,1,2], target = 0;
+console.log("Is Target present or not:",search(nums,target));
