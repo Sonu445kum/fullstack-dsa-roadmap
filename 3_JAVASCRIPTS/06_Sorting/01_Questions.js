76c53ea0c4aa7c479307f68bda2df1d4ae9d4045
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-04 17:34:31 +0530

    this keyword

--- 3_JAVASCRIPTS/06_Sorting/01_Questions.js
+++ 3_JAVASCRIPTS/06_Sorting/01_Questions.js
@@ -135,17 +135,19 @@
 // console.log("Merge Sort:",mergeSort(arr));
 
 // quickSort
-function quickSort(arr){
-    if(arr.length <= 1) return arr;
+// function quickSort(arr){
+//     if(arr.length <= 1) return arr;
 
-    let pivot = arr[arr.length - 1];
-    let left = [] , right = [];
+//     let pivot = arr[arr.length - 1];
+//     let left = [] , right = [];
 
-    for(let i =0; i<arr.length - 1; i++){
-        if(arr[i] < pivot) left.push(arr[i]);
-        else right.push(arr[i]);
-    }
-    return [...quickSort(left) , pivot , ...quickSort(right)];
-};
-let arr = [3,5,8,4];
-console.log("Quick Sort:",quickSort(arr));
+//     for(let i =0; i<arr.length - 1; i++){
+//         if(arr[i] < pivot) left.push(arr[i]);
+//         else right.push(arr[i]);
+//     }
+//     return [...quickSort(left) , pivot , ...quickSort(right)];
+// };
+// let arr = [3,5,8,4];
+// console.log("Quick Sort:",quickSort(arr));
+
+
---
+++ 3_JAVASCRIPTS/17_Web-Dev/call_apply_bind.js
@@ -0,0 +1,8 @@
+// this method
+const user = {
+    name :"Sonu",
+    greet: function(){
+        console.log(this.name);
+    }
+}
+user.greet(); // sonu
