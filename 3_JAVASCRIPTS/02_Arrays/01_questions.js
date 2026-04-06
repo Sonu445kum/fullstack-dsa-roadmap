10536965832499f62febdf981d94a8a6493b423d
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-06 22:12:01 +0530

    Solved:sortArrayByParity

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1956,12 +1956,45 @@
 
 // Output: 1
 
-var peakIndexInMountainArray = function(arr) {
-    for(let i=1; i<arr.length; i++){
-        if(arr[i] > arr[i-1] && arr[i] >arr[i+1]){
-            return i;
+// var peakIndexInMountainArray = function(arr) {
+//     for(let i=1; i<arr.length; i++){
+//         if(arr[i] > arr[i-1] && arr[i] >arr[i+1]){
+//             return i;
+//         }
+//     }
+// };
+// let arr = [0,2,1,0];
+// console.log("peakIndexInMountainArray:",peakIndexInMountainArray(arr));
+
+// Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+// Return any array that satisfies this condition.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,1,2,4]
+// Output: [2,4,3,1]
+// Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+var sortArrayByParity = function(nums) {
+    let ans = [];
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] % 2 === 0){
+            ans.push(nums[i]);
         }
     }
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] % 2 !==0){
+            ans.push(nums[i]);
+        }
+    }
+    return ans;
 };
-let arr = [0,2,1,0];
-console.log("peakIndexInMountainArray:",peakIndexInMountainArray(arr));
+let nums = [3,1,2,4];
+console.log("sortArrayByParity:",sortArrayByParity(nums));
