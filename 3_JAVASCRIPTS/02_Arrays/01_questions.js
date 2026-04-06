68d45d9549afbbb6cfbb95909113aa5824e9b2ed
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-06 22:44:22 +0530

    solved:sortArrayByParityII

--- 3_JAVASCRIPTS/02_Arrays/01_questions.js
+++ 3_JAVASCRIPTS/02_Arrays/01_questions.js
@@ -1982,19 +1982,55 @@
 // Input: nums = [0]
 // Output: [0]
 
-var sortArrayByParity = function(nums) {
-    let ans = [];
-    for(let i =0; i<nums.length; i++){
-        if(nums[i] % 2 === 0){
-            ans.push(nums[i]);
+// var sortArrayByParity = function(nums) {
+//     let ans = [];
+//     for(let i =0; i<nums.length; i++){
+//         if(nums[i] % 2 === 0){
+//             ans.push(nums[i]);
+//         }
+//     }
+//     for(let i =0; i<nums.length; i++){
+//         if(nums[i] % 2 !==0){
+//             ans.push(nums[i]);
+//         }
+//     }
+//     return ans;
+// };
+// let nums = [3,1,2,4];
+// console.log("sortArrayByParity:",sortArrayByParity(nums));
+
+// Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+// Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+// Return any answer array that satisfies this condition.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,2,5,7]
+// Output: [4,5,2,7]
+// Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+// Example 2:
+
+// Input: nums = [2,3]
+// Output: [2,3]
+
+var sortArrayByParityII = function(nums) {
+    let arr = new Array(nums.length);
+    let even = 0;
+    let odd = 1;
+    for(let num of nums){
+        if(num % 2 === 0){
+            arr[even] =  num;
+            even += 2;
+        }else{
+            arr[odd] = num;
+            odd += 2;
         }
     }
-    for(let i =0; i<nums.length; i++){
-        if(nums[i] % 2 !==0){
-            ans.push(nums[i]);
-        }
-    }
-    return ans;
+    return arr;
 };
-let nums = [3,1,2,4];
-console.log("sortArrayByParity:",sortArrayByParity(nums));
+let nums = [4,2,5,7];
+console.log("sortArrayByParityII:",sortArrayByParityII(nums));
