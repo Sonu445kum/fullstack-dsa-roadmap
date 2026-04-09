1115d34c86cbdde368f023d26c618f81661fd074
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-09 19:39:25 +0530

    Solved:House Robber

--- 3_JAVASCRIPTS/18_Dp/01_Questions.js
+++ 3_JAVASCRIPTS/18_Dp/01_Questions.js
@@ -1,12 +1,25 @@
 // Find the  fibbonacci number:
-function fib(n , memo = {}){
-    if(n in memo) return memo[n];
-    if(n <= 1) return n;
-    memo[n] = fib(n-1, memo) + fib(n-2, memo);
-    return memo[n];
-}
-let n=13;
-console.log("Fibbonacci Number:",fib(n));
+// this is Memoizations Form;
+// function fib(n , memo = {}){
+//     if(n in memo) return memo[n];
+//     if(n <= 1) return n;
+//     memo[n] = fib(n-1, memo) + fib(n-2, memo);
+//     return memo[n];
+// }
+// let n=13;
+// console.log("Fibbonacci Number:",fib(n));
+
+// Tabulations form:
+// function fib(n){
+//     let dp = new Array(n+1).fill(0);
+//     dp[1] = 1;
+//     for(let i=2; i<=n; i++){
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+//     return dp[n];
+// };
+// let n=13;
+// console.log("Fibbonacci:",fib(n));
 
 
 // You are climbing a staircase. It takes n steps to reach the top.
@@ -42,3 +55,30 @@ console.log("Fibbonacci Number:",fib(n));
 // };
 // let n = 3;
 // console.log("Total Ways to CLimbs Stairs:",climbStairs(n));
+
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+
+var rob = function(nums) {
+    let n = nums.length;
+    if(n === 1) return nums[0];
+    let dp = new Array(n+1).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0] , nums[1]);
+    for(let i=2; i<=n; i++){
+        dp[i] = Math.max(nums[i]+dp[i-2] ,dp[i-1]);
+    }
+    return dp[n-1];
+};
+let nums = [1,2,3,1];
+console.log("Robs:",rob(nums));
