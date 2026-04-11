0930a2e408a48616ff682f21fc120f294d8acd60
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-11 16:04:44 +0530

    Solved:inorderTraversal

--- 3_JAVASCRIPTS/18_Tree/01_TreeTraversal.js
+++ 3_JAVASCRIPTS/18_Tree/01_TreeTraversal.js
@@ -1,54 +1,80 @@
-class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+// class TreeNode {
+//     constructor(val) {
+//         this.val = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
+// //  Create Tree
+// //        1
+// //       / \
+// //      2   3
+// //     / \
+// //    4   5
+
+// let root = new TreeNode(1);
+// root.left = new TreeNode(2);
+// root.right = new TreeNode(3);
+// root.left.left = new TreeNode(4);
+// root.left.right = new TreeNode(5);
+
+// //  Preorder: Root → Left → Right
+// function preorder(root) {
+//     if (!root) return;
+//     process.stdout.write(root.val + " ");
+//     preorder(root.left);
+//     preorder(root.right);
+// }
+
+// //  Inorder: Left → Root → Right
+// function inorder(root) {
+//     if (!root) return;
+//     inorder(root.left);
+//     process.stdout.write(root.val + " ");
+//     inorder(root.right);
+// }
+
+// // Postorder: Left → Right → Root
+// function postorder(root) {
+//     if (!root) return;
+//     postorder(root.left);
+//     postorder(root.right);
+//     process.stdout.write(root.val + " ");
+// }
+
+// //  Display Output
+// console.log("Preorder:");
+// preorder(root);
+
+// console.log("\nInorder:");
+// inorder(root);
+
+// console.log("\nPostorder:");
+// postorder(root);
+
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,null,2,3]
+
+// Output: [1,3,2]
+
+// Explanation:
+var inorderTraversal = function(root){
+    let result = [];
+    function dfs(node){
+        // base case
+        if(!node) return;
+        dfs(node.left);
+        result.push(node.val);
+        dfs(node.right);
     }
+    dfs(root);
+    return result;
 }
-
-//  Create Tree
-//        1
-//       / \
-//      2   3
-//     / \
-//    4   5
-
-let root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-
-//  Preorder: Root → Left → Right
-function preorder(root) {
-    if (!root) return;
-    process.stdout.write(root.val + " ");
-    preorder(root.left);
-    preorder(root.right);
-}
-
-//  Inorder: Left → Root → Right
-function inorder(root) {
-    if (!root) return;
-    inorder(root.left);
-    process.stdout.write(root.val + " ");
-    inorder(root.right);
-}
-
-// Postorder: Left → Right → Root
-function postorder(root) {
-    if (!root) return;
-    postorder(root.left);
-    postorder(root.right);
-    process.stdout.write(root.val + " ");
-}
-
-//  Display Output
-console.log("Preorder:");
-preorder(root);
-
-console.log("\nInorder:");
-inorder(root);
-
-console.log("\nPostorder:");
-postorder(root);
+let root = [1,null,2,3];
+console.log("inorderTraversal:",inorderTraversal(root));
