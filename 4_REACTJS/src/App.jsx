ba9862d5d4af61545c747b1edf66c9666dadbe33
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-13 23:30:29 +0530

    Render Props

--- 4_REACTJS/src/App.jsx
+++ 4_REACTJS/src/App.jsx
@@ -108,4 +108,18 @@ function withLoading(Component) {
 const UserListWithLoading = withLoading(UserList);
 
 
+function MouseTracker({ render }) {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  return (
+    <div onMouseMove={(e) => setPos({ x: e.clientX, y: e.clientY })}>
+      {render(pos)}
+    </div>
+  );
+}
+
+// Usage
+<MouseTracker render={(pos) => (
+  <h1>Mouse: {pos.x}, {pos.y}</h1>
+)} />
 
