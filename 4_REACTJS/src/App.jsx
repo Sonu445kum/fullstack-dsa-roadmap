1af61b1ade2841d36951bc819dfc1916b4cd751f
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-13 23:31:53 +0530

    Portals

--- 4_REACTJS/src/App.jsx
+++ 4_REACTJS/src/App.jsx
@@ -145,3 +145,12 @@ function TabPanel({ index, children }) {
   return active === index ? <div>{children}</div> : null;
 }
 
+import { createPortal } from "react-dom";
+
+function Modal({ children }) {
+  return createPortal(
+    <div className="modal">{children}</div>,
+    document.getElementById("modal-root")
+  );
+}
+
