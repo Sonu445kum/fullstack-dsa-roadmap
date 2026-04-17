9825ef184780578beac4e708df89c350597179f4
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-17 11:39:22 +0530

    Solved:findLonely

--- 3_JAVASCRIPTS/08_Hash_Table/01_Questions.js
+++ 3_JAVASCRIPTS/08_Hash_Table/01_Questions.js
@@ -450,3 +450,37 @@
 // };
 // let letters = ["c","f","j"], target = "a";
 // console.log("nextGreatestLetter:",nextGreatestLetter(letters , target));
+
+// You are given an integer array nums. A number x is lonely when it appears only once, and no adjacent numbers (i.e. x + 1 and x - 1) appear in the array.
+
+// Return all lonely numbers in nums. You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [10,6,5,8]
+// Output: [10,8]
+// Explanation: 
+// - 10 is a lonely number since it appears exactly once and 9 and 11 does not appear in nums.
+// - 8 is a lonely number since it appears exactly once and 7 and 9 does not appear in nums.
+// - 5 is not a lonely number since 6 appears in nums and vice versa.
+// Hence, the lonely numbers in nums are [10, 8].
+// Note that [8, 10] may also be returned.
+
+var findLonely = function(nums) {
+    let freq = {};
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1;
+    }
+    let ans =[];
+    for(let [key,value] of Object.entries(freq)){
+        let res = Number(key);
+        if(value === 1 && !freq[res - 1] && !freq[res + 1]){
+            ans.push(res);
+        }
+    }
+    return ans;
+};
+let nums = [10,6,5,8];
+console.log("findLonely Number:",findLonely(nums));
