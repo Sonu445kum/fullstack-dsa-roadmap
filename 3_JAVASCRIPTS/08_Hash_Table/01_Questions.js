496888305630288dfcfbea2658be4fb000492b13
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-17 13:13:44 +0530

    Solved:numberOf

--- 3_JAVASCRIPTS/08_Hash_Table/01_Questions.js
+++ 3_JAVASCRIPTS/08_Hash_Table/01_Questions.js
@@ -554,19 +554,51 @@
 // Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
 // Note that "cdc" is also a longest uncommon subsequence.
 
-var findLUSlength = function(a, b) {
-    if( a=== b) return -1;
-    if( a === reverse(b)) return -1;
-    if( a !== reverse(b) && a.length > b.length) return a.length;
-    if(a !== reverse(b)) return b.length;
-};
-let a = "aba", b = "cdc";
-console.log("findLUSLength:",findLUSlength(a , b));
-function reverse(s){
-    let rev = "";
-    for(let i = s.length - 1; i>=0; i--){
-        rev += s[i];
+// var findLUSlength = function(a, b) {
+//     if( a=== b) return -1;
+//     if( a === reverse(b)) return -1;
+//     if( a !== reverse(b) && a.length > b.length) return a.length;
+//     if(a !== reverse(b)) return b.length;
+// };
+// let a = "aba", b = "cdc";
+// console.log("findLUSLength:",findLUSlength(a , b));
+// function reverse(s){
+//     let rev = "";
+//     for(let i = s.length - 1; i>=0; i--){
+//         rev += s[i];
+//     }
+//     return rev;
+// }
+
+// You are given 2 integer arrays nums1 and nums2 of lengths n and m respectively. You are also given a positive integer k.
+
+// A pair (i, j) is called good if nums1[i] is divisible by nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1).
+
+// Return the total number of good pairs.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,3,4], nums2 = [1,3,4], k = 1
+
+// Output: 5
+
+// Explanation:
+
+// The 5 good pairs are (0, 0), (1, 0), (1, 1), (2, 0), and (2, 2).
+
+var numberOfPairs = function(nums1, nums2, k) {
+    let count =0;
+    for(let i =0; i<nums1.length; i++){
+        for(let j = 0; j<nums2.length; j++){
+            if(nums1[i] %( nums2[j]*k) === 0){
+                count++;
+            }
+        }
     }
-    return rev;
-}
+    return count;
+};
+let nums1 = [1,3,4], nums2 = [1,3,4], k = 1;
+console.log("numberOfPairs:",numberOfPairs(nums1 , nums2 , k));
 
