c288e1192449cbd72371322fe02d49d1d973c9ec
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-21 12:03:19 +0530

    Solved:similarPairs

--- 3_JAVASCRIPTS/03_Strings/01_Questions.js
+++ 3_JAVASCRIPTS/03_Strings/01_Questions.js
@@ -950,19 +950,52 @@
 // - "00000" consists only of digits, so its value is 0.
 // Hence, the maximum value is 5, of "alic3".
 
-var maximmumValue = function(strs){
-    let max =0;
-    for(let word of strs){
-        let hasLetter = /[a-z]/i.test(word);
-        let value;
-        if(hasLetter){
-            value = word.length;
-        }else{
-            value = Number(word);
+// var maximmumValue = function(strs){
+//     let max =0;
+//     for(let word of strs){
+//         let hasLetter = /[a-z]/i.test(word);
+//         let value;
+//         if(hasLetter){
+//             value = word.length;
+//         }else{
+//             value = Number(word);
+//         }
+//         max  = Math.max(max , value);
+//     }
+//     return max;
+// };
+// let strs = ["alic3","bob","3","4","00000"];
+// console.log("maximumValue:",maximmumValue(strs));
+
+// You are given a 0-indexed string array words.
+
+// Two strings are similar if they consist of the same characters.
+
+// For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+// However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+// Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i] and words[j] are similar.
+
+ 
+
+// Example 1:
+
+// Input: words = ["aba","aabb","abcd","bac","aabc"]
+// Output: 2
+// Explanation: There are 2 pairs that satisfy the conditions:
+// - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'. 
+// - i = 3 and j = 4 : both words[3] and words[4] only consist of characters 'a', 'b', and 'c'. 
+
+var similarPairs = function(words){
+    let map = new Map();
+    let count = 0;
+    for(let word of words){
+        let key = [...new Set(word)].sort().join("");
+        if(map.has(key)){
+            count += map.get(key);
         }
-        max  = Math.max(max , value);
+        map.set(key , (map.get(key) || 0) + 1);
     }
-    return max;
+    return count;
 };
-let strs = ["alic3","bob","3","4","00000"];
-console.log("maximumValue:",maximmumValue(strs));
+let words = ["aba","aabb","abcd","bac","aabc"];
+console.log("SimilarPairs:",similarPairs(words));
