719007a2f7bfed57590cf3c952e129ea57140c94
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-27 22:50:07 +0530

    Solved:minimumOperations

--- 3_JAVASCRIPTS/05_Math/01_Questions.js
+++ 3_JAVASCRIPTS/05_Math/01_Questions.js
@@ -3327,10 +3327,42 @@
 // Output: 6
 // Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
 
-var pivotInteger = function(n) {
-    let total = n*(n+1)/2;
-    let x  = Math.sqrt(total);
-    return Number.isInteger(x) ? x : -1;
-};
-let n = 8;
-console.log("PivotInteger:",pivotInteger(n));
+// var pivotInteger = function(n) {
+//     let total = n*(n+1)/2;
+//     let x  = Math.sqrt(total);
+//     return Number.isInteger(x) ? x : -1;
+// };
+// let n = 8;
+// console.log("PivotInteger:",pivotInteger(n));
+
+// You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+
+// Return the minimum number of operations to make all elements of nums divisible by 3.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+
+// Output: 3
+
+// Explanation:
+
+// All array elements can be made divisible by 3 using 3 operations:
+
+// Subtract 1 from 1.
+// Add 1 to 2.
+// Subtract 1 from 4.
+
+var minimumOperations = function(nums){
+    let count =0;
+    for(let num of nums){
+        if(num % 3 !== 0){
+            count++;
+        }
+    }
+    return count;
+}
+let nums =[1,2,3,4];
+console.log("MinimumOperations:",minimumOperations(nums));
